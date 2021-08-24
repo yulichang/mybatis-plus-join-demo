1715f7f6f597360b16722e5053e39a146d729741
bjdys <git@bjdys.com>
2021-08-24 09:56:35 +0800

    升级 -> 1.2.0.Beta5

--- join-demo/src/main/java/com/github/yulichang/join/Application.java
+++ join-demo/src/main/java/com/github/yulichang/join/Application.java
@@ -1,6 +1,5 @@
 package com.github.yulichang.join;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
